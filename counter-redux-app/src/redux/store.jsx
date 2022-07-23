f9ca4507cbc5a4bb8e2e8bbf28341810f5df270b
MaiMeselhi <Mai.Meselhi@gmail.com>
2022-07-23 13:50:01 +0200

    add ReduxDevTool extension

--- counter-redux-app/src/redux/store.jsx
+++ counter-redux-app/src/redux/store.jsx
@@ -1,4 +1,6 @@
 import { createStore } from "redux";
 import counterReducer from "./reducers/counterReducer";
-const store = createStore(counterReducer);
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(counterReducer, enhancer());
 export default store;
